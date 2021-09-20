d33692b4561e7aec8828b5e4455ce154a7e94d5c
Greugreu <Juillet-1991>
2021-09-20 15:55:40 +0200

    Bubble filter algo

--- src/Controller/AlgoController.php
+++ src/Controller/AlgoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use phpDocumentor\Reflection\Types\Boolean;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,13 +14,14 @@ class AlgoController extends AbstractController
      */
     public function index(): Response
     {
+
         return $this->render('algo/index.html.twig', [
-            'result' => $this->fact(5),
+            'result' => json_encode($this->bubble([55, 2, 8])),
             'controller_name' => 'AlgoController',
         ]);
     }
 
-    protected function fact($nmbr)
+    protected function fact(int $nmbr): int
     {
         if ($nmbr !== 0)
         {
@@ -27,5 +29,25 @@ class AlgoController extends AbstractController
         } else {
             return 1;
         }
+    }
+
+    protected function bubble(array $arr): array
+    {
+        $isSwap = true;
+        while($isSwap === true) {
+            $isSwap = false;
+            $length = count($arr);
+            for ($i = 0; $i < $length - 1; $i++)
+            {
+                if ($arr[$i] > $arr[$i + 1])
+                {
+                    $temp = $arr[$i];
+                    $arr[$i] = $arr[$i + 1];
+                    $arr[$i + 1 ] = $temp;
+                    $isSwap = true;
+                }
+            }
+        }
+        return $arr;
     }
 }
